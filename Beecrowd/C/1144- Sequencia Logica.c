6eecf010ea7e520542df1f420609dda0530bd3d4
João Pedro Cardoso <joaool1301@gmail.com>
2025-11-04 20:54:49 -0300

    Upload: 1144 - Sequência Lógica

---
+++ Beecrowd/C/1144- Sequencia Logica.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x;
+    scanf("%d", &x);
+    
+    for(int i=1; i<=x; i++){
+        printf("%d %d %d\n", i, i*i, i*i*i);
+        printf("%d %d %d\n", i, (i*i)+1, (i*i*i)+1);
+    }
+
+    return 0;
+}
