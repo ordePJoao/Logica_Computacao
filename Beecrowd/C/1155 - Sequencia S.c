972fa64ba0d43bf4f7c0e89c62d275abbf0704c8
João Pedro Cardoso de Oliveira <joaool1301@gmail.com>
2025-11-09 20:26:31 -0300

    Upload: 1155 - Sequência

---
+++ Beecrowd/C/1155 - Sequencia S.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    double s = 0.0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        s = s + (1.0 / i);
+    }
+
+    printf("%.2lf\n", s);
+
+    return 0;
+}
