70afe55487df37aaccc55092d254199603db51e4
João Pedro Cardoso de Oliveira <joaool1301@gmail.com>
2026-03-19 01:40:38 -0300

    Upload: 1004 - Produto Simples

---
+++ Beecrowd/Cpp/1004 - Produto Simples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin >> a >> b;
+
+    cout << "PROD = " << a*b << endl;
+
+    return 0;
+}
